fd321fee7ab33adb9bcdc2b4a93af61ec18ace69
Stardust333 <92229638+Stardust333@users.noreply.github.com>
2022-09-21 19:41:46 +0530

    Add files via upload

---
+++ components/footer/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './FooterStyles.css'
+import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa'
+
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <div className="container">
+                <div className="top">
+                    <h3>WeddingKnots.</h3>
+                    <div className="social">
+                        <FaFacebook className='icon' />
+                        <FaInstagram className='icon' />
+                        <FaTwitter className='icon' />
+                        <FaPinterest className='icon' />
+                        <FaYoutube className='icon' />
+                    </div>
+                </div>
+                <div className="bottom">
+                    <div className="left">
+                        <ul>
+                            <li>About</li>
+                            <li>Reviews</li>
+                            <li>Photo gallery</li>
+                            
+                            <li>Advertising</li>
+                        </ul>
+                    </div>
+                    <div className="right">
+                        <ul>
+                            <li>Contact</li>
+                            
+                            <li>Policy</li>
+                            <li>Privacy</li>
+                            <li>Pricing</li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
